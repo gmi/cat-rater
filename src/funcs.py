63495bb5d262d85741143e6392eedf180a403ed1
vulger <blokongithub@gmail.com>
2025-03-07 21:45:54 +0000

    fixes

--- src/funcs.py
+++ src/funcs.py
@@ -5,6 +5,6 @@ def get_2_random():
     cat2 = db.get_random_cat()
     while cat2 == cat1:
         print("woah, 1 in 10k chance of 2 same cats")
-        cat2 == db.get_random_cat()
+        cat2 = db.get_random_cat()
     return [cat1[1], cat2[1]]
 
